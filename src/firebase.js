922628823e0313b2d3c67148adb537d6f1c0612b
Jatin Jasoliya <>
2021-02-07 18:12:44 -0500

    Clean-up Project

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyADPpXa2c9k9L58gaF3D0SCUaYrIuob5n8",
+  authDomain: "clone-40661.firebaseapp.com",
+  projectId: "clone-40661",
+  storageBucket: "clone-40661.appspot.com",
+  messagingSenderId: "924636189778",
+  appId: "1:924636189778:web:114801a6e55e4af8e07b8b",
+  measurementId: "G-QS6QH9YEBG",
+};
